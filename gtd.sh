c563d8ddf130ca6002d05624d56a40226f24639c
Tristan Chase <tristan.m.chase@gmail.com>
2020-11-23 19:52:55 -0500

    Convert script to new layout
    * Fix grepping problem
    * Unset errexit and pipefail
    These cause the script to exit when grep doesn't find anything (exits
    1).

--- gtd.sh
+++ gtd.sh
@@ -1,21 +1,65 @@
-#!/bin/bash
-set -e
-#
-# gtd.sh
-#
-# Tristan M. Chase
-# Created: Mon 2018-04-09 01:24:12
-#
-# Description:
+#!/usr/bin/env bash
+
 #-----------------------------------
+# Usage Section
 
-# Variables
+#<usage>
+#//Usage: gtd [ {-d|--debug} ] [ {-h|--help} | <options>] [<arguments>]
+#//Description: Get things done!
+#//Examples: gtd foo; gtd --debug bar
+#//Options:
+#//	-d --debug	Enable debug mode
+#//	-h --help	Display this help message
+#</usage>
 
+#<created>
+# Created: Mon 2018-04-09 01:24:12
+# Tristan M. Chase <tristan.m.chase@gmail.com>
+#</created>
 
-# Dependencies
+#<depends>
+# Depends on:
+#  list
+#  of
+#  dependencies
+#</depends>
 
+#-----------------------------------
+# TODO Section
 
-# Process
+#<todo>
+# * Insert script
+# * Clean up stray ;'s
+# * Modify command substitution to "$(this_style)"
+# * Rename function_name() to function __function_name__ /\w+\(\)
+# * Rename $variables to "${_variables}" /\$\w+/s+1 @v vEl,{n
+# * Check that _variable="variable definition" (make sure it's in quotes)
+# * Update usage, description, and options section
+# * Update dependencies section
+
+# DONE
+
+#</todo>
+
+#-----------------------------------
+# License Section
+
+#<license>
+# Put license here
+#</license>
+
+#-----------------------------------
+# Runtime Section
+
+#<main>
+# Initialize variables
+#_temp="file.$$"
+
+# List of temp files to clean up on exit (put last)
+#_tempfiles=("${_temp}")
+
+# Put main script here
+function __main_script__ {
 
 # Remember starting directory
 _startdir="$(pwd)"
@@ -31,11 +75,11 @@ if [[ -e todos.txt ]]; then
 fi
 
 # Update #tags @Locations projects done canceled (using sed and grep combo; examples in todo.sh)
-grep -o '#[a-zA-Z0-9-]\+*' *.txt | sed -n 's/\(.*:\)\(#.*\)/\2/p' | sort | uniq > tags
-grep -o '@[a-zA-Z0-9-]\+*' *.txt | sed -n 's/\(.*:\)\(@.*\)/\2/p' | sort | uniq > locations
+grep -o '#[a-zA-Z0-9-]\+*' *.txt | sed -n 's/\(.*:\)\(#.*\)/\2/p' | sort -u > tags
+grep -o '@[a-zA-Z0-9-]\+*' *.txt | sed -n 's/\(.*:\)\(@.*\)/\2/p' | sort -u > locations
 
 # Search through .txt files for items marked with a "*"
-grep -n "^ *\*" *.txt | sed 's:\*:\t\t\*:' > todos.txt
+grep -n '^\s\+*\*' *.txt | sed 's/\*/    \*/' > todos.txt
 
 # Open the file for exploitation
 vim todos.txt
@@ -50,3 +94,56 @@ cd $_startdir
 #   gtd -t "blah"
 # and nudge to correct file
 
+} #end __main_script__
+#</main>
+
+#-----------------------------------
+# Local functions
+
+#<functions>
+function __local_cleanup__ {
+	:
+}
+#</functions>
+
+#-----------------------------------
+# Source helper functions
+if [[ -e ~/.functions.sh ]]; then
+	source ~/.functions.sh
+fi
+
+#-----------------------------------
+# Get some basic options
+# TODO Make this more robust
+#<options>
+if [[ "${1:-}" =~ (-d|--debug) ]]; then
+	__debugger__
+elif [[ "${1:-}" =~ (-h|--help) ]]; then
+	__usage__
+fi
+#</options>
+
+#-----------------------------------
+# Bash settings
+# Same as set -euE -o pipefail
+#<settings>
+#set -o errexit
+set -o nounset
+set -o errtrace
+#set -o pipefail
+IFS=$'\n\t'
+#</settings>
+
+#-----------------------------------
+# Main Script Wrapper
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
+	trap __traperr__ ERR
+	trap __ctrl_c__ INT
+	trap __cleanup__ EXIT
+
+	__main_script__
+
+
+fi
+
+exit 0
